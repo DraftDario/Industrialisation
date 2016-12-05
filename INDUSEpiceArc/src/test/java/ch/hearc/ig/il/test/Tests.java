2eda5254e89aa6a3c1efc468035ffbf508c3f882
DraftDario <dario.mosca@he-arc.ch>
2016-12-05 15:02:52 +0100

    StartTests
    Not good yet

---
+++ INDUSEpiceArc/src/test/java/ch/hearc/ig/il/test/Tests.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.hearc.ig.il.test;
+
+import ch.hearc.ig.odi.epicearc.business.DeliveryDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author dario.mosca
+ */
+public class Tests {
+    
+        @Test
+    public final void testDeliveryDate() {
+        List<DeliveryDate> dates = new ArrayList();
+        dates.add();
+
+        assertTrue();
+    }
+    
+}
